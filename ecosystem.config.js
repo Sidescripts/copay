cd35f5630cfece5c0da2afdb83aff6b33cab5603
sidescripts <dubyernest14@gmail.com>
2026-03-02 10:29:48 +0100

    fix: hosting issue

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    apps: [{
+      name: 'vitron-app',
+      script: './server/app.js',  // Your entry file
+      instances: 'max',
+      exec_mode: 'cluster',
+      watch: false,
+      ignore_watch: ['node_modules', 'logs', '.git'],
+      max_memory_restart: '1G',
+      autorestart: true,
+      out_file: './logs/out.log',
+      error_file: './logs/err.log',
+      merge_logs: true, 
+      time: true,
+      env: {
+        NODE_ENV: 'production',
+      }
+    }]
+};
